5111b5af2dc1dff97f3b55e240f540a598d27a14
Sejal Sinha <sejalsinha@codeyourself.org>
2025-02-13 12:34:43 +0530

    Modified sounds

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ makeSound(event.key);
 
 function makeSound(key) {
 
-  
 switch(key) {
   case "w":
     var crash = new Audio("sounds/crash.mp3");
@@ -39,8 +38,8 @@ switch(key) {
     break;
     
   case "d":
-    var wolf = new Audio("sounds/wolf.mp3");
-    wolf.play();
+    var crash = new Audio("sounds/crash.mp3");
+    crash.play();
     break;
     
   case "j":
@@ -57,57 +56,11 @@ switch(key) {
     var tom4 = new Audio("sounds/tom-4.mp3");
     tom4.play();
     break;
+
+    default: console.log(buttonInnerHTML);
 }
 
-var audio = new Audio("sounds/hen.mp3");
-audio.play();
-
-switch(key) {
-
-case "w":
-var crash = new Audio("sounds/crash.mp3");
-crash.play();
-break;
-
-case "a":
-var cat = new Audio("sounds/snare.mp3");
-cat.play();
-break;
-
-case "s":
-var snare = new Audio("sounds/snare.mp3");
-snare.play();
-break;
-
-case "d":
-var wolf = new Audio("sounds/wolf.mp3");
-wolf.play();
-break;
-
-case "j":
-var tom2 = new Audio("sounds/tom-2.mp3");
-tom2.play();
-break;
-
-case "k":
-var tom3 = new Audio("sounds/tom-3.mp3");
-tom3.play();
-break;
-
-case "l":
-var tom4 = new Audio("sounds/tom-4.mp3");
-tom4.play();
-break;
-
-default: console.log(buttonInnerHTML);
-
 }
-  
-
-
-}
-
-
 
 document.addEventListener("keypress", function() {
 alert("Key was pressed!");
